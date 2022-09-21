de0bb48809de75b5ef8141390e7f09d1fca32ae4
Leonardo <leonardoluz10@hotmail.com>
2022-09-21 10:43:59 -0300

    Criando um laço while para o uso do onclik

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ function tocaSomPom() {
 
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+// Enquanto
+while (contador < 9) {
+    listaDeTeclas[contador].onclick = tocaSomPom;
+    contador = contador + 1;
+    console.log(contador);
+}

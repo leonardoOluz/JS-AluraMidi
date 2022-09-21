f63914c4c4188414ca14bfc130f8f938614d8307
Leonardo <leonardoluz10@hotmail.com>
2022-09-21 13:07:51 -0300

    Concluído a função de chamada ao tocaSom usando o .classList().

--- main.js
+++ main.js
@@ -3,14 +3,18 @@ function tocaSom(idElementoAudio) {
     document.querySelector(idElementoAudio).play();
 }
 
-
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 let contador = 0;
-
 // Enquanto
 while (contador < listaDeTeclas.length) {
-    listaDeTeclas[contador].onclick = tocaSom;
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    // template string
+    const idAudio = `#som_${instrumento}`;
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    };
     contador = contador + 1;
-    console.log(contador);
 }

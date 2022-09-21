a07e133da45938af22e36564d2227d7cd0d82b3e
Leonardo <leonardoluz10@hotmail.com>
2022-09-21 15:38:59 -0300

    Trabalhando com laço de repetição (Para) => FOR

--- main.js
+++ main.js
@@ -6,15 +6,30 @@ function tocaSom(idElementoAudio) {
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 let contador = 0;
-// Enquanto
+
+// Laço de repetição (Para) 
+for (var cont = 0; cont < listaDeTeclas.length; cont++) {
+    const tecla = listaDeTeclas[cont];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; // template string
+    tecla.onclick = function () {
+        tocaSom(idAudio)
+    }
+
+}
+
+/*
+// Laço de repetição (Enquanto)
 while (contador < listaDeTeclas.length) {
 
     const tecla = listaDeTeclas[contador];
-    const instrumento = tecla.classList[1];
-    // template string
-    const idAudio = `#som_${instrumento}`;
+    const instrumento = tecla.classList[1];    
+    const idAudio = `#som_${instrumento}`; // template string
+
     tecla.onclick = function () {
         tocaSom(idAudio);
     };
     contador = contador + 1;
-}
+
+} 
+*/
